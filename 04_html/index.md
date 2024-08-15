c13caaf82095f9f6a2aa5fbb584f6e75318624ac
Greg Wilson <gvwilson@third-bit.com>
2024-08-15 06:43:51 -0400

    feat: outline HTML generation lesson
    closes #4

--- 04_html/index.md
+++ 04_html/index.md
@@ -1,3 +1,17 @@
 # Serving HTML
 
-FIXME
+-   Introduce the view part of MVC and use [Jinja][jinja] templates
+    -   Alternative [htpy][htpy] is more amenable to debugging but harder to read
+-   Create [`exceptions.py`](./exceptions.py) to hold exceptions
+    -   Common parent for all application exceptions to make catching easier
+-   Introduction `fmt=whatever` query parameter
+    -   Considered a bad practice by some: API endpoint should return a single data format
+    -   Doing it this way prevents us using [FastAPI][fastapi]'s built-in support for template rendering
+-   Load templates from filesystem in [`views.py`](./views.py)
+-   Pass in data as named argument
+-   Use the sample template for single-row and multi-row views
+-   No changes to [`model.py`](./model.py)
+
+[fastapi]: https://fastapi.tiangolo.com/
+[htpy]: https://htpy.dev/
+[jinja]: https://jinja.palletsprojects.com/
---
+++ 04_html/server.py
@@ -0,0 +1,60 @@
+"""Serve data from data model layer."""
+
+from fastapi import FastAPI, HTTPException
+
+import model
+import views
+
+from exceptions import AppException
+
+
+HEARTBEAT = {"message": "alive"}
+FMT_HTML = "html"
+FMT_JSON = "json"
+
+
+def handle_format(data, fmt, view):
+    """Handle format conversion."""
+    if fmt == FMT_JSON:
+        return data
+    elif fmt == FMT_HTML:
+        return view(data)
+    raise HTTPException(status_code=415, detail=f"Unsupported format {fmt}")
+
+
+def make_server():
+    """Build application and configure routes."""
+    app = FastAPI()
+
+    @app.get("/")
+    def root(fmt: str = FMT_JSON):
+        try:
+            return handle_format(model.all_staff(), fmt, views.all_staff)
+        except AppException as exc:
+            raise HTTPException(status_code=404, detail=f"Error serving all staff: {exc}")
+
+    @app.get("/heartbeat")
+    def heartbeat(fmt: str = FMT_JSON):
+        try:
+            return handle_format(HEARTBEAT, fmt, views.heartbeat)
+        except AppException as exc:
+            raise HTTPException(status_code=404, detail=f"Error serving heartbeat: {exc}")
+
+    @app.get("/col/{name}")
+    def column(name: str, fmt: str = FMT_JSON):
+        try:
+            return handle_format(model.column(name), fmt, views.column)
+        except AppException as exc:
+            raise HTTPException(status_code=404, detail=f"Error serving column {name}: {exc}")
+
+    @app.get("/row/{staff_id}")
+    def row(staff_id: int, fmt: str = FMT_JSON):
+        try:
+            return handle_format(model.row(staff_id), fmt, views.row)
+        except AppException as exc:
+            raise HTTPException(status_code=404, detail=f"Error serving row {staff_id}: {exc}")
+
+    return app
+
+
+app = make_server()
