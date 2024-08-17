bc782a43dd6c6caa621f4e6cb0fe27fc55d63562
Greg Wilson <gvwilson@third-bit.com>
2024-08-17 12:36:25 -0400

    feat: previewing CSS packages
    -   add `98_css` with `index.html` and some Alpine to preview options
    -   modify `bin/render.py` to copy symlinks
    -   include `data` by copying `.csv` and `.json` files
    -   prevent Jekyll running by adding .nojekyll file
    -   Also prevent copying of .venv

--- bin/util.py
+++ bin/util.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 
-SUFFIXES = {".css", ".html", ".js", ".md", ".py"}
+SUFFIXES = {".css", ".csv", ".html", ".js", ".json", ".md", ".py"}
 
 
 def find_files(opt, root_skips=[]):
@@ -11,20 +11,43 @@ def find_files(opt, root_skips=[]):
     return {
         filepath: _read_file(filepath)
         for filepath in Path(opt.root).glob("**/*.*")
-        if _is_interesting(filepath, root_skips)
+        if _is_interesting_file(filepath, root_skips)
     }
 
 
-def _is_interesting(filepath, root_skips):
+def find_symlinks(opt, root_skips=[]):
+    """Collect all interesting files."""
+    return [
+        filepath
+        for filepath in Path(opt.root).glob("**/*")
+        if _is_interesting_symlink(filepath, root_skips)
+    ]
+
+
+def _is_interesting_file(filepath, root_skips):
     """Is this file worth checking?"""
-    result = (
-        (not str(filepath).startswith(".")) and
-        filepath.is_file() and
-        (filepath.suffix in SUFFIXES) and
-        (not str(filepath.parent.name).startswith(".")) and
-        (not any(str(filepath).startswith(s) for s in root_skips))
-    )
-    return result
+    if not filepath.is_file():
+        return False
+    if str(filepath).startswith("."):
+        return False
+    if filepath.suffix not in SUFFIXES:
+        return False
+    if str(filepath.parent.name).startswith("."):
+        return False
+    if any(str(filepath).startswith(s) for s in root_skips):
+        return False
+    return True
+
+
+def _is_interesting_symlink(filepath, root_skips):
+    """Is this symlink worth checking?"""
+    if not filepath.is_symlink():
+        return False
+    if str(filepath).startswith("."):
+        return False
+    if any(str(filepath).startswith(s) for s in root_skips):
+        return False
+    return True
 
 
 def _read_file(filepath):
