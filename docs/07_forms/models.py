cc12879d80eca6751d6c998919e35036d96a888e
Greg Wilson <gvwilson@third-bit.com>
2024-08-17 14:20:58 -0400

    feat: outline form submission lesson
    closes #6

--- docs/07_forms/models.py
+++ docs/07_forms/models.py
@@ -2,6 +2,7 @@
 
 import os
 from pypika import Query, Table
+import pypika.functions as fn
 import sqlite3
 
 from util import ModelException, dict_factory
@@ -10,9 +11,10 @@ from util import ModelException, dict_factory
 ENV_VAR = "DB"
 
 
-def connect():
+def connect(path = None):
     """Connect to database."""
-    path = os.getenv(ENV_VAR)
+    if path is None:
+        path = os.getenv(ENV_VAR)
     if not path:
         raise ModelException(f"Environment variable {ENV_VAR} not set")
     connection = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
@@ -20,10 +22,23 @@ def connect():
     return connection
 
 
+def add_staff(personal, family):
+    """Add staff member, generating staff ID."""
+    staff = Table("staff")
+    subquery = Query.from_(staff).select(fn.Max(staff.staff_id) + 1)
+    query = Query.into(staff).insert(subquery, personal, family)
+    try:
+        connection = connect()
+        cursor = connection.execute(str(query))
+        connection.commit()
+    except sqlite3.DatabaseError as exc:
+        raise ModelException(str(exc))
+
+
 def all_staff():
     """Get all staff."""
     staff = Table("staff")
-    query = Query.from_(staff).select("staff_id", "personal", "family")
+    query = Query.from_(staff).select(staff.staff_id, staff.personal, staff.family)
     try:
         connection = connect()
         cursor = connection.execute(str(query))
